57e708d10090953150398a443e0b46412ee16207
KlevtsovAndrey <66443327+KlevtsovAndrey@users.noreply.github.com>
2021-08-10 17:29:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int current_balance = 2_000_000_000;
         int transfer_amount = 500_000_000;
-        int total = current_balance - transfer_amount;
+        int total = current_balance + transfer_amount;
         System.out.println(total);
     }
 }
